9ddee677a68174692ea30bf0e3efdc18a0ba5202
kylesteiner <kyleste@live.com>
2014-07-21 11:15:11 -0700

    file structure changes

--- src/TreesAndGraphs.java
+++ src/TreesAndGraphs.java
@@ -1,12 +1,9 @@
-package TreesAndGraphs;
+import java.util.LinkedList;
+import java.util.Queue;
 
+import org.jgrapht.DirectedGraph;
 
-public class Tree {
-	private final Node root;
-
-    public Tree() {
-        root = new Node();
-    }
+public class TreesAndGraphs {
 
     /*
      * Problem 1
@@ -35,11 +32,41 @@ public class Tree {
 	}
 
 	/*
+	 * Problem 2
+	 */
+	public static boolean isDirectedRoute(Vertex start, Vertex end, DirectedGraph<Vertex, Edge> g) {
+		for (Vertex v : g.vertexSet())
+			v.visited = false;
+
+		Vertex v;
+		Queue<Vertex> q = new LinkedList<Vertex>();
+		q.add(start);
+		while(!q.isEmpty()) {
+			v = q.remove();
+			for (Edge e : g.outgoingEdgesOf(v)) {
+				Vertex v2 = e.v;
+			}
+		}
+		return false;
+	}
+
+	/*
 	 * Default Node class
 	 */
 	private static class Node {
 		private int data;
 		private Node left;
 		private Node right;
+	}
+
+	/*
+	 * Default Edge & Vertex classes
+	 */
+	private static class Vertex {
+		private boolean visited;
+	}
+
+	private static class Edge {
+		private Vertex v;
 	}
 }
