05336166359428d6ad0f084ed17f1a2662108e68
kylesteiner <kyleste@live.com>
2014-08-12 11:27:06 -0700

    Trees and Graphs Problem 8

--- src/TreesAndGraphs.java
+++ src/TreesAndGraphs.java
@@ -181,4 +181,31 @@ public class TreesAndGraphs {
 			return ancestorHelper(c1, c2, root.right);
 		}
 	}
+
+	/*
+	 * Problem 8
+	 */
+	public static boolean isSubtree(TreeNode t1, TreeNode t2) {
+		if (t2 == null) {
+			return true;
+		} else if (t1 == null) {
+			return false;
+		} else if (t1.data == t2.data && isSameTree(t1, t2)) {
+			return true;
+		} else {
+			return isSubtree(t1.left, t2) || isSubtree(t1.right, t2);
+		}
+	}
+
+	private static boolean isSameTree(TreeNode t1, TreeNode t2) {
+		if (t1 == null && t2 == null) {
+			return true;
+		} else if (t1 == null || t2 == null) {
+			return false;
+		} else {
+			return t1.data == t2.data &&
+				isSameTree(t1.left, t2.left) &&
+				isSameTree(t1.right, t2.right);
+		}
+	}
 }
